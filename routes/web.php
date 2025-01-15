027db175af78038c5505a37ed64eaf91c1f5a035
talithazulfha <zh.talitha13@gmail.com>
2025-01-15 11:38:19 +0700

    ppid

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\PpidController;
 use App\Http\Controllers\ProfilController;
 use App\Http\Controllers\SejarahController;
 
@@ -16,8 +18,12 @@ Route::get('/berita', function () {
     return view('berita');
 });
 
-Route::get('/galeri', function () {
-    return view('galeri');
+Route::get('/foto', function () {
+    return view('foto');
+});
+
+Route::get('/detailFoto', function () {
+    return view('detailFoto');
 });
 
 Route::get('/kegiatan', function () {
@@ -31,3 +37,9 @@ Route::get('/download', function () {
 Route::get('/ppid', function () {
     return view('ppid');
 });
+
+Route::get('/profilPpid', [PpidController::class, 'profilPpid'])->name('ppid.profil');
+Route::get('/informasiSertaMerta', [PpidController::class, 'informasiSertaMerta'])->name('ppid.serta-merta');
+Route::get('/informasiTersediaSetiapSaat', [PpidController::class, 'informasiTersediaSetiapSaat'])->name('ppid.setiap-saat');
+Route::get('/informasiBerkala', [PpidController::class, 'informasiBerkala'])->name('ppid.berkala');
+Route::get('/informasiDikecualikan', [PpidController::class, 'informasiDikecualikan'])->name('ppid.dikecualikan');
